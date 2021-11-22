02a98a40bca56bd824030925ba21026afbf04488
iflar <classicsience@gmail.com>
2021-11-22 18:19:13 -0500

    added Rating functionality ie: controller

--- RestaurantRater/Controllers/RatingController.cs
+++ RestaurantRater/Controllers/RatingController.cs
@@ -1,13 +1,38 @@
-﻿using System;
+﻿using RestaurantRater.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace RestaurantRater.Controllers
 {
     public class RatingController : ApiController
     {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+
+        [HttpPost]
+        public async Task<IHttpActionResult> CreateRating([FromBody] Rating model)
+        {
+            if (model is null)
+                return BadRequest("Request body cannot be null");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var restaurantEntity = await _context.Restaurants.FindAsync(model.RestaurantId);
+
+            if (restaurantEntity is null)
+                return BadRequest("The ID does not exist");
+
+            restaurantEntity.Ratings.Add(model);
+            if (await _context.SaveChangesAsync() == 1)
+                return Ok("Resturant added!!!!");
+
+
+            return InternalServerError();
+        }
     }
 }
